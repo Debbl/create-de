ed9fa35ba7e6d8210c335c6d99eec77ac2bf8b60
Debbl <me@aiwan.run>
2023-04-13 14:40:04 +0800

    feat: add git init

---
+++ src/gitCommitInit.ts
@@ -0,0 +1,14 @@
+import { execSync } from "child_process";
+
+export default function gitCommitInit(execPath: string) {
+  const execSyncGit = (command: string) => {
+    return execSync(command, { cwd: execPath });
+  };
+  try {
+    execSyncGit("git --version");
+    execSyncGit('git init && git add . && git commit -m "chore: init"');
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+}
