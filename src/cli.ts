48cd7ade3731eb9b9a31fec2af5f490a0da1d5f6
Brendan Dash <me@aiwan.run>
2026-02-10 15:57:53 +0800

    chore: line

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 import { cac } from 'cac'
 import consola from 'consola'
-import { version } from '../package.json'
 import { run } from '.'
+import { version } from '../package.json'
 
 const cli = cac('create-de')
 
